56ea070aff60c99c4737028e31f6aa9757ac476d
Carlos Chao <chao@factory.generalsoftwareinc.com>
2024-08-21 12:19:00 -0400

    feat: add advice response using ai

--- src/app/api/advices/route.tsx
+++ src/app/api/advices/route.tsx
@@ -1,20 +1,18 @@
-import { advices } from "@/lib/_mocks";
+import {
+  generateRandomAdvice,
+  generateRandomAIAdvice,
+} from "@/lib/server/services/advices";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const ai = searchParams.get("ai");
-  console.log(ai);
-
-  // generate a ramdom number index using the length of advices
-  const randomIndex = Math.floor(Math.random() * advices.length);
-
-  try {
-    const advice = advices[randomIndex];
-    return new Response(advice);
-  } catch (e) {
-    return new Response("Internal server error", {
-      status: 500,
-    });
+  let response = "";
+  if (ai === "true") {
+    response = await generateRandomAIAdvice();
+  } else {
+    response = await generateRandomAdvice();
   }
+
+  return new Response(response);
 }
---
+++ src/lib/server/services/advices.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { generateText } from "ai";
+
+import { advices } from "@/lib/_mocks";
+
+const google = createGoogleGenerativeAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
+
+export const generateRandomAdvice = async (): Promise<string> => {
+  // generate a ramdom number index using the length of advices
+  try {
+    const randomIndex = Math.floor(Math.random() * advices.length);
+    const advice = advices[randomIndex];
+    return advice;
+  } catch {
+    return "Ocurrió un error en al generar el consejo";
+  }
+};
+
+export const generateRandomAIAdvice = async (): Promise<string> => {
+  const model = google("models/gemini-1.5-flash");
+  const { text } = await generateText({
+    model: model,
+    system: "Eres un desarrollador web Senior con 10 años de experiencia.",
+    prompt:
+      "¿Qué consejo le darías a un desarrollador web que recién empieza?. El consejo debería ser una frase de un parrafo corto maximo 120 caracteres. El formato de respuesta debe ser un texto plano.",
+  });
+
+  return text;
+};
